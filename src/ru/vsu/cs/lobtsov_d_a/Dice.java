719ec93dc9a0d625c25f53199009a8de6aa5394b
Дмитрий <143889235+shelf08@users.noreply.github.com>
2023-12-18 14:25:05 +0300

    Update Dice.java

--- src/ru/vsu/cs/lobtsov_d_a/Dice.java
+++ src/ru/vsu/cs/lobtsov_d_a/Dice.java
@@ -1,2 +1,11 @@
-package ru.vsu.cs.lobtsov_d_a;public class Dice {
+package ru.vsu.cs.lobtsov_d_a;
+import java.util.Random;
+
+class Dice {
+
+    // nextInt() gives an int from 0 to 5, adding 1
+    // to make it in the range of 1-6
+    int roll() {
+        return new Random().nextInt(6) + 1;
+    }
 }
